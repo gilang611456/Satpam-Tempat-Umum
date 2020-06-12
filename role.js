b0317d30078dfd1dda14753fd5d13aafdcde4f56
gilang611456 <65783878+gilang611456@users.noreply.github.com>
2020-06-13 01:46:51 +0700

    Update role.js

--- role.js
+++ role.js
@@ -1,14 +1,4 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log('Pinging');
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+
 
 
 // Import constructors, configuration and login the client
